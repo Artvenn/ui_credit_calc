0972acdb7ac915bcd400a7c8e45661c4fcdccc19
AzizDevelop <jafarov.develop@gmail.com>
2024-07-31 19:20:28 +0500

    changed the sdk version and keyboard type

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: MyHomePage(),
-      debugShowCheckedModeBanner: false,
-    )
-  );
+  runApp(MaterialApp(
+    home: MyHomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
 class MyHomePage extends StatelessWidget {
@@ -20,49 +18,60 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text('My home page', style: TextStyle(color: Colors.white),)),
+        title: const Center(
+            child: Text(
+          'My home page',
+          style: TextStyle(color: Colors.white),
+        )),
         backgroundColor: Colors.green,
       ),
       drawer: Drawer(
-        child: ListView(
-        padding: const EdgeInsets.all(20),
-        children: const [
-        ]
-      )),
+          child:
+              ListView(padding: const EdgeInsets.all(20), children: const [])),
       body: ListView(
         padding: const EdgeInsets.all(20),
-        children:  [
+        children: [
           TextField(
+            keyboardType: TextInputType.number,
             decoration: InputDecoration(
               label: Text('Sell price'),
-              prefixIcon: Icon(Icons.price_change,),
+              prefixIcon: Icon(
+                Icons.price_change,
+              ),
             ),
             controller: _sellPriceCtrl,
             onChanged: (val) {
               final parsedNum = double.parse(val);
-              final creditAmount = (parsedNum+(parsedNum/100*30));
+              final creditAmount = (parsedNum + (parsedNum / 100 * 30));
               _credAmountCtrl.text = creditAmount.toString();
-              _firstFondCtrl.text = (creditAmount/100*30).toString();
+              _firstFondCtrl.text = (creditAmount / 100 * 30).toString();
             },
           ),
-          SizedBox(height: 5,),
+          SizedBox(
+            height: 5,
+          ),
           TextField(
             readOnly: true,
             decoration: InputDecoration(
               label: Text('Credit amount'),
-              prefixIcon: Icon(Icons.price_change,),
+              prefixIcon: Icon(
+                Icons.price_change,
+              ),
             ),
             controller: _credAmountCtrl,
           ),
-          SizedBox(height: 5,),
+          SizedBox(
+            height: 5,
+          ),
           TextField(
             decoration: InputDecoration(
               label: Text('First fond'),
-              prefixIcon: Icon(Icons.price_change,),
+              prefixIcon: Icon(
+                Icons.price_change,
+              ),
             ),
             controller: _firstFondCtrl,
-            onChanged:
-            (val) {
+            onChanged: (val) {
               final firstFound = double.parse(val);
               final monthCount = double.parse(_creditMonthCountCtrl.text);
               final ostatok = double.parse(_credAmountCtrl.text) - firstFound;
@@ -73,16 +82,21 @@ class MyHomePage extends StatelessWidget {
               _everyMonthFondCtrl.text = '${ostatok / monthCount}';
             },
           ),
-          SizedBox(height: 5,),
+          SizedBox(
+            height: 5,
+          ),
           TextField(
             decoration: InputDecoration(
               label: Text('Credit month count'),
-              prefixIcon: Icon(Icons.price_change,),
+              prefixIcon: Icon(
+                Icons.price_change,
+              ),
             ),
             controller: _creditMonthCountCtrl,
             onChanged: (val) {
               final monthCount = double.parse(val);
-              final ostatok = double.parse(_credAmountCtrl.text) - double.parse(_firstFondCtrl.text);
+              final ostatok = double.parse(_credAmountCtrl.text) -
+                  double.parse(_firstFondCtrl.text);
               if (monthCount <= 0) {
                 _everyMonthFondCtrl.text = '';
                 return;
@@ -90,18 +104,24 @@ class MyHomePage extends StatelessWidget {
               _everyMonthFondCtrl.text = '${ostatok / monthCount}';
             },
           ),
-          SizedBox(height: 5,),
+          SizedBox(
+            height: 5,
+          ),
           TextField(
             readOnly: true,
             decoration: InputDecoration(
               label: Text('Every month fond'),
-              prefixIcon: Icon(Icons.price_change,),
+              prefixIcon: Icon(
+                Icons.price_change,
+              ),
             ),
             controller: _everyMonthFondCtrl,
           ),
-
-      ],),
-      floatingActionButton: FloatingActionButton(onPressed: () => print('the button is pressed'),),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => print('the button is pressed'),
+      ),
     );
   }
 }
